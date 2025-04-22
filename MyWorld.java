63b7b233263792b4bb72e1287268980d087bc28a
nthnj <349045377@gapps.yrdsb.ca>
2025-04-22 14:03:24 -0400

    Add seal and movement mechanics

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,10 @@
-import greenfoot.*;
+    import greenfoot.*;
 
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Hero seal = new Hero();
+        addObject(seal, 100, 100);
     }
 }
